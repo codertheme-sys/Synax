b44c8d0f49257b1f10e509cd720c238a2c6740b0
cumhur381 <cumhur381@gmail.com>
2026-01-15 10:06:40 +0300

    Add LiveChat script integration - user needs to add license ID

--- pages/_app.js
+++ pages/_app.js
@@ -116,33 +116,55 @@ function MyApp({ Component, pageProps }) {
     };
   }, [router.pathname]);
 
-  // Load Live Chat Script
+  // Load LiveChat Script
   useEffect(() => {
-    // Check if live chat script is already loaded
     if (typeof window === 'undefined') return;
     
-    // You can add your live chat script here
-    // Example for Tawk.to:
-    // const script = document.createElement('script');
-    // script.async = true;
-    // script.src = 'https://embed.tawk.to/YOUR_PROPERTY_ID/YOUR_WIDGET_ID';
-    // script.charset = 'UTF-8';
-    // script.setAttribute('crossorigin', '*');
-    // document.head.appendChild(script);
+    // Check if LiveChat is already loaded
+    if (window.LC_API || window.__lc) return;
     
-    // Example for Crisp:
-    // window.$crisp = [];
-    // window.CRISP_WEBSITE_ID = "YOUR_WEBSITE_ID";
-    // (function() {
-    //   const d = document;
-    //   const s = d.createElement("script");
-    //   s.src = "https://client.crisp.chat/l.js";
-    //   s.async = 1;
-    //   d.getElementsByTagName("head")[0].appendChild(s);
-    // })();
+    // LiveChat script - Replace YOUR_LICENSE_ID with your actual license ID
+    // You can find your license ID in LiveChat dashboard: Settings > Chat Widget > Installation
+    window.__lc = window.__lc || {};
+    window.__lc.license = process.env.NEXT_PUBLIC_LIVECHAT_LICENSE_ID || 'YOUR_LICENSE_ID';
     
-    // Add your live chat script code above this comment
-    // The button will automatically detect and open the chat widget
+    (function(n, t, c) {
+      function i(n) {
+        return e._h ? e._h.apply(null, n) : e._q.push(n);
+      }
+      var e = {
+        _q: [],
+        _h: null,
+        _v: "2.0",
+        on: function() {
+          i(["on", c.call(arguments)]);
+        },
+        once: function() {
+          i(["once", c.call(arguments)]);
+        },
+        off: function() {
+          i(["off", c.call(arguments)]);
+        },
+        get: function() {
+          if (!e._h) throw new Error("[LiveChatWidget] You can't use getters before load.");
+          return i(["get", c.call(arguments)]);
+        },
+        call: function() {
+          i(["call", c.call(arguments)]);
+        },
+        init: function() {
+          var n = t.createElement("script");
+          n.async = !0;
+          n.type = "text/javascript";
+          n.src = "https://cdn.livechatinc.com/tracking.js";
+          t.head.appendChild(n);
+        }
+      };
+      !n.__lc.asyncInit && e.init();
+      n.LiveChatWidget = n.LiveChatWidget || e;
+    })(window, document, [].slice);
+    
+    setLiveChatLoaded(true);
   }, []);
 
   // Check for user session
